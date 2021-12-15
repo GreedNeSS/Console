f8589d28f329cc247fbb8dfa5d4c3b10b46f3371
GreedNeSS <ruslan199126@gmail.com>
2021-12-15 19:36:43 +0300

    Добавьте файлы проекта.

---
+++ 1-console-buffer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1_console_buffer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
