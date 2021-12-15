9725f17df8c901513c9bd642cf2357b29559fa85
GreedNeSS <ruslan199126@gmail.com>
2021-12-16 00:14:56 +0300

    Console.Read()

---
+++ 3-read/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _3_read
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Title = "Read()";
+            string data = "";
+            int charCode;
+
+            do
+            {
+                charCode = Console.Read();
+                char symbol = Convert.ToChar(charCode);
+                data += symbol;
+
+            } while (charCode != 13);
+
+            Console.WriteLine("Вывод в консоль полученных данных: {0}", data);
+            Console.ReadLine();
+        }
+    }
+}
